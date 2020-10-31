1ee44e2f55c5af8041312be803cd421ba6df30c1
Felipe Macedo <felipeaamacedo1@gmail.com>
2020-10-31 09:48:11 +0100

    addition of RP_Fwd at kin-pair.ts

--- kin-pair.ts
+++ kin-pair.ts
@@ -38,7 +38,7 @@ function RR_Fwd(c:number, b:number, PHI:number, PSI:number, PHI_d:number, PSI_d:
 	)
 }
 
-function RR_Inv(b:number, c:number, Xa:number, Ya:number, Xq:number, Yq:number, X_d:number, Y_d:number, X_dd:number, Y_dd:number):number[]{
+function RR_Inv(b:number, c:number, Xa:number, Ya:number, Xq:number, Yq:number, X_d:number, Y_d:number, X_dd:number, Y_dd:number, I:number):number[]{
 	//INVERSE KINEMATICS (POSITION CALCULATION)
 	let X = Xa - Xq
        	let Y = Ya - Yq
@@ -50,30 +50,42 @@ function RR_Inv(b:number, c:number, Xa:number, Ya:number, Xq:number, Yq:number, 
 	let t1 = (- B + Math.sqrt(B**2 - A*C))/A
 	let t2 = (- B - Math.sqrt(B**2 - A*C))/A
 
-	let PHI_1 = 2*Math.atan(t1)
-	let PHI_2 = 2*Math.atan(t2)
+	if(I==1){
+		let t = t1
+	}else{
+		let t = t2
+	}
 
-	let U1:number = (X - c*Math.cos(PHI_1))/b
-	let V1:number = (Y - c*Math.sin(PHI_1))/b	
+	let PHI = 2*Math.atan(t1)
 
-	let U2:number = (X - c*Math.cos(PHI_2))/b
-	let V2:number = (Y - c*Math.sin(PHI_2))/b	
+	let U:number = (X - c*Math.cos(PHI))/b
+	let V:number = (Y - c*Math.sin(PHI))/b	
 
-	let PSI_1:number = Math.atan2(V1,U2)
-	let PSI_2:number = Math.atan2(V2,U2)
+	let PSI:number = Math.atan2(V,U)
 	
 	// INVERSE KINEMATICS (VELOCITY CALCULATION)
 
-	return(
-	[
-		PHI_1,
-		PHI_2,
-		PSI_1,
-		PSI_2
-	]
-	)
+	return [PHI, PSI]
 }
 
+function RP_Fwd(a:number, Xq:number, Yq:number, r:number, PHI:number, r_d:number, PHI_d:number, r_dd:number, PHI_dd:number):number[]{
+	
+	//FORWARD KINEMATICS (POSITION CALCULATION)
+	let X:number = -a*Math.sin(PHI) + r*Math.cos(PHI)
+	let Y:number = r*Math.sin(PHI) + a*Math.cos(PHI)
+	
+	let Xa:number = Xq + X
+	let Ya:number = Yq + Y
 
+	// FORWARD KINEMATICS (VELOCITY CALCULATION)
+	let Xa_d:number = ( - a*Math.cos(PHI) - r*Math.sin(PHI))*PHI_d + Math.cos(PHI)*r_d
+	let Ya_d:number = (r*Math.cos(PHI) - a*Math.sin(PHI))*PHI_d + Math.sin(PHI)*r_d
+
+	// FORWARD KINEMATICS (ACCELERATION CALCULATION)
+	let Xa_dd:number = ( - a*Math.cos(PHI) - r*Math.sin(PHI))*PHI_dd + Math.cos(PHI)*r_dd + (a*PHI_d*Math.sin(PHI) - r*PHI_d*Math.cos(PHI) - r_d*Math.sin(PHI)) * PHI_d + ( - PHI_d*Math.sin(PHI))*r_d
+	let Ya_dd:number = (r*Math.cos(PHI) - a*Math.sin(PHI))*PHI_dd + Math.sin(PHI)*r_dd + (- a*PHI_d*Math.cos(PHI) - r*PHI_d*Math.sin(PHI) + r_d*Math.cos(PHI))*PHI_d + (PHI_d*Math.cos(PHI))*r_d
+
+	return [Xa, Ya, Xa_d, Ya_d, Xa_dd, Ya_dd]
+}
 
 console.log(RR_Fwd(c,b,PHI,PSI,PHI_d,PSI_d,PHI_dd,PSI_dd))
